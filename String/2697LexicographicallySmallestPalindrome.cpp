31f7a274cebea9ac549c817eb55ec6fe9d723f4b
Wenzhe Jiao <wenzhejiao@gmail.com>
2023-08-03 00:35:08 -0400

    Solved problem 2697 Category: String Difficulty: Easy

---
+++ String/2697LexicographicallySmallestPalindrome.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+
+        for (int i = 0, j = s.size() - 1; i <= j; i++, j--) {
+            if (s[i] < s[j]) {
+                s[j] = s[i];
+            }
+            else if (s[i] > s[j]) {
+                s[i] = s[j];
+            }
+        }
+        return s;
+    }
+};
