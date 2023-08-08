3db494daaeda0453c560f20c1021c5d0102b546f
Wenzhe Jiao <wenzhejiao@gmail.com>
2023-08-07 23:30:53 -0400

    Solved problem 2716 Category: Hash Table Difficulty: Easy

---
+++ Hash Table/2716MinimizeStringLength.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    int minimizedStringLength(string s) {
+        unordered_set<char> set;
+        for (auto& ch : s) {
+            set.insert(char);
+        }
+        return set.size();
+    }
+};
