52b95ac0e76cabf188933b35c2917baa1caacfd3
Wenzhe Jiao <wenzhejiao@gmail.com>
2023-08-22 22:14:28 -0400

    Solved problem 2586 Category: String Difficulty: Easy

---
+++ String/2586CountTheNumberOfVowelStringsInRange.cpp
@@ -0,0 +1,20 @@
+#include<vector>
+#include<string>
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int vowelStrings(vector<string>& words, int left, int right) {
+        int ret = 0;
+        for (int i = left; i <= right; i++) {
+            int len = words[i].size() - 1;
+            if ((words[i][0] == 'a' || words[i][0] == 'e' ||
+                words[i][0] == 'i' || words[i][0] == 'o' || words[i][0] == 'u') &&
+                (words[i][len] == 'a' || words[i][len] == 'e' ||
+                    words[i][len] == 'i' || words[i][len] == 'o' || words[i][len] == 'u'))
+                ret++;
+        }
+        return ret;
+    }
+};
